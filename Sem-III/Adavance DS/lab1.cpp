16767d7b723897446eb28c1a138549db68b8fd3f
2025b01010374 <ritu.25b01010374@abes.ac.in>
2026-08-13 09:59:45 +0530

    Lab-01

---
+++ Sem-III/Adavance DS/lab1.cpp
@@ -0,0 +1,129 @@
+// 1.1 Implement Stack using Array
+//1.2 Implement Stack using Linked List
+//1.3 Expression coversion using stack i). i).Infix to Postfix
+//1.4 Evaluate an Expression using stack : 1) Evaluate Postfix Exp
+
+
+#include <bits/stdc++.h>
+using namespace std;
+
+class ArrayStack {
+    public:
+      int* stack;
+      int size;
+      int top;
+      ArrayStack (int n) {
+        size=n;
+        stack = new int[size];
+        top = -1;
+      }
+      void push(int x) {
+        if(top == size -1){
+            cout << "Stack Overflow" << endl;
+            return;
+        }
+        stack[++top] = x;
+      }
+
+      int pop() {
+        if (isEmpty()) {
+            cout << "Stack is Empty"<< endl;
+            return -1;
+        }
+
+            return stack[top--];
+            // int temp = stack[top];
+            // top--;
+            // return temp;
+
+        }
+       int peek() {
+        if (isEmpty()){
+            cout << "Stack is Empty" << endl;
+            return -1;
+        }
+         return stack[top];        
+       }
+       bool isEmpty(){
+        return top == -1;
+       }
+
+       void display() {
+        if(isEmpty())
+            cout <<"Stack is Empty, nothing to display."<< endl;
+            else
+            {
+
+                for(int i=top; i>=0; i--)
+                {
+                    cout <<"\n"<<stack[i];
+                }
+            }    
+       }
+      };
+
+      int main() {
+          int n,x;
+          cout << "Enter the size of Stack : " << endl;
+          cin>>n;
+          ArrayStack s(n);
+          int choice = 0;
+          cout << "\n  STACK OPERATIONS USING ARRAY  \n" ;
+          cout << "\n-----\n" << endl;
+         
+          while(choice !=5){
+
+            cout <<"\n\n choose one from below options : \n ";
+            cout << "\n 1.Push \n 2. Pop\n 3.Display \n 4. Peek \n 5. Exit";
+            cout << "\n Enter your Choice: \n";
+            cin>>choice;
+
+            switch(choice){
+
+                case 1:{
+                    cout<< "Enter the element to be inserted in Stack : " << endl;
+                    cin>>x;
+                    s.push(x);
+                    s.display();
+                    break;
+                }
+
+                case 2:
+                {
+                    cout <<"The Deleted item is: " <<s.pop();
+                    s.display();
+                    break;
+                }
+
+                case 3 :
+                {
+                    s.display();
+                    break;
+                }
+                
+                case 4 :
+                {
+                    s.peek();
+                    break;
+                }
+
+                case 5 :
+                {
+                    cout<< "Exiting...";
+                    break;
+                }
+
+                default :
+                {
+                    cout << "Please Enter valid choice";
+                }
+            };
+
+          }
+
+          return 0;
+
+      }
+      
+
+      
