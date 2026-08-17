e2acf967902911c7e5736d4c379f055ad2438ec6
2025b01010374 <ritu.25b01010374@abes.ac.in>
2026-08-17 12:22:07 +0530

    Static Keyword

---
+++ Sem-III/OOPS/staticKeyword.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+
+using namespace std;
+
+class  Account 
+{
+    public: 
+        int balance;
+        static float roi;
+
+        public:
+         void setBalance(int b){
+            balance = b;
+            cout <<balance;
+         }
+};
+
+float Account::roi = 3.5f;
+
+int main()
+{
+    // clrscr();
+    Account a1, a2;
+    // a1.setBalance(5);
+    cout << Account::roi;
+    // getch();
+    
+}
