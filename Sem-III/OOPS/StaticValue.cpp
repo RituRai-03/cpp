fb5695730a8c7c442647065e944ae2fb8daa40e9
2025b01010374 <ritu.25b01010374@abes.ac.in>
2026-08-18 14:44:09 +0530

    Updated Static Keyword

--- Sem-III/OOPS/StaticValue.cpp
+++ Sem-III/OOPS/StaticValue.cpp
@@ -1,17 +1,26 @@
-#include <iostream>
+// 
+
+#include <bits/stdc++.h>
 using namespace std;
 
-void fun(){
-    int x;
-    static int y;
-    cout << x<< endl << y;
-    x++;
-    y++;
-}
-
-int main(){
-    fun();
-    fun();
-    fun();
-    return 0;
+class Account {
+    private:
+     int balance;
+     static float roi;
+     
+    public:
+     void setBalance(int b){
+         balance = b;
+     }
+     static void setroi (float r){
+        roi = r;
+        cout << roi;
+     }
+};
+float Account::roi=3.5f;
+int main() {
+	
+    //Account a1,a2;
+   // a1.setroi(4.5f);
+  Account::setroi(4.5f);
 }
