52eb411b6c007364075aa3843e2332d13ccf424f
2025b01010374 <ritu.25b01010374@abes.ac.in>
2026-08-18 11:13:34 +0530

    Lab Experiment -02

--- Sem-III/OOPS/lab2.cpp
+++ Sem-III/OOPS/lab2.cpp
@@ -18,4 +18,51 @@ class Addition {
         return a + b;
       }
 
+      //Call by Value
+
+      int addByValue(int x, int y){
+        return x + y;   
+      }
+
+      //Call by Reference
+
+      int addByReference(int &x, int &y){
+        int temp = x + y;
+        x = x + 10;
+        y = y + 10;
+        return temp;
+      }
+
+      //Call by Address
+
+      int addByAddress(int *x, int *y){
+        int temp = *x + *y;
+        *x = *x + 10;
+        *y = *y + 10;
+        return temp;
+      }   
+
+};
+
+int main(){
+  Addition obj;
+
+  cout << "Enter two numbers: ";
+  int a,b;
+  cin >> a >> b;
+  
+ 
+cout << "\nBy Value Sum: " << obj.addByValue(a, b) << endl;
+cout << "a = " << a << ", b = " << b << endl;
+
+cout << "\nBy Reference Sum: " << obj.addByReference(a, b) << endl;
+cout << "a = " << a << ", b = " << b << endl;
+
+cout << "\nBy Address Sum: " << obj.addByAddress(&a, &b) << endl;
+cout << "a = " << a << ", b = " << b << endl;
+
+return 0;
+
 }
+ 
+
