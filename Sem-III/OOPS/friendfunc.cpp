f8c34556a4c68006f61b696466a481b508eda3bd
2025b01010374 <ritu.25b01010374@abes.ac.in>
2026-08-25 15:21:40 +0530

    Friend Function Example

--- Sem-III/OOPS/friendfunc.cpp
+++ Sem-III/OOPS/friendfunc.cpp
@@ -1,10 +1,45 @@
-//Friend Function
-#include <iostream>
-using namespace std;
+// //Friend Function
+// #include <iostream>
+// using namespace std;
 
-// class Complex
+// // class Complex
+// // {
+// //     private:
+// //      int a ,b;
+// //     public:
+// //      void SetData(int x, int y)
+// //      {
+// //         a = x;
+// //         b = y;
+// //      }
+
+// //      void ShowData()
+// //      {
+// //         cout << a << endl << b;
+// //      }
+
+// //      friend void fun(Complex);
+// //     };
+
+// //      void fun (Complex c)
+// //     {
+// //         cout << c.a+c.b;
+// //     }
+
+// //     int main(){
+// //         ;
+// //         Complex c1;
+// //         c1.SetData(5,4);
+// //         fun(c1);
+        
+// //     }
+
+
+//     //Write a friend funciton to add two complex numbers
+
+//     class Complex
 // {
-//     private:
+//      private:
 //      int a ,b;
 //     public:
 //      void SetData(int x, int y)
@@ -13,68 +48,70 @@ using namespace std;
 //         b = y;
 //      }
 
+
 //      void ShowData()
 //      {
 //         cout << a << endl << b;
 //      }
 
-//      friend void fun(Complex);
+//      friend Complex fun(Complex , Complex);
 //     };
 
-//      void fun (Complex c)
+//      Complex fun (Complex c , Complex d)
 //     {
-//         cout << c.a+c.b;
+//         Complex temp;
+//         temp.a=c.a+d.a;
+//         temp.b=c.b+d.b;
+//         return temp;
+        
 //     }
 
 //     int main(){
-//         ;
-//         Complex c1;
+        
+//         Complex c1, c2, c3;
 //         c1.SetData(5,4);
-//         fun(c1);
+//         c2.SetData(2,3);
+//         c3 = fun(c1,c2);
+//         c3.ShowData();
         
 //     }
 
+    #include <iostream>
+    using namespace std;
 
-    //Write a friend funciton to add two complex numbers
+    class B;
+    class A {
+        int a;
+     public:
+      void setData(int x)
+      {
+        a=x;
 
-    class Complex
-{
-     private:
-     int a ,b;
-    public:
-     void SetData(int x, int y)
-     {
-        a = x;
-        b = y;
-     }
-
-
-     void ShowData()
-     {
-        cout << a << endl << b;
-     }
-
-     friend Complex fun(Complex , Complex);
+      }
+      friend void fun(A,B);
     };
 
-     Complex fun (Complex c , Complex d)
-    {
-        Complex temp;
-        temp.a=c.a+d.a;
-        temp.b=c.b+d.b;
-        return temp;
-        
-    }
+    class B {
+        int b;
+     public:
+      void setData(int y)
+      {
+        b=y;
+      }
+    friend void fun(A,B);
+};
 
-    int main(){
-        
-        Complex c1, c2, c3;
-        c1.SetData(5,4);
-        c2.SetData(2,3);
-        c3 = fun(c1,c2);
-        c3.ShowData();
-        
-    }
+void fun(A o1, B o2)
+{
+    cout << o1.a+o2.b;
+}
 
+int main(){
+    A obj1;
+    B obj2;
+    obj1.setData(2);
+    obj2.setData(3);
+    fun(obj1, obj2);
+}
     
     
