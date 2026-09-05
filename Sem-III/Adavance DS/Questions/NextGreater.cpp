344cb6b09ef523b5a02e26940234a4dc70faa7d0
2025b01010374 <ritu.25b01010374@abes.ac.in>
2026-09-05 22:24:12 +0530

    Next Greater Number gfg

--- Sem-III/Adavance DS/Questions/NextGreater.cpp
+++ Sem-III/Adavance DS/Questions/NextGreater.cpp
@@ -1,2 +1,39 @@
 //Next Greater Element 
 
+#include <iostream>
+#include <stack>
+#include <vector>
+using namespace std;
+
+vector<int> nextGreaterElement(vector<int>& arr) {
+    int n = arr.size();
+    vector<int> result(n, -1);  // initialize with -1
+    stack<int> st;              // stack to store elements
+
+    // Traverse from right to left
+    for (int i = n - 1; i >= 0; i--) {
+        // Pop smaller or equal elements
+        while (!st.empty() && st.top() <= arr[i]) {
+            st.pop();
+        }
+
+        // If stack not empty, top is next greater
+        if (!st.empty()) {
+            result[i] = st.top();
+        }
+
+        // Push current element
+        st.push(arr[i]);
+    }
+
+    return result;
+}
+
+// Example usage
+int main() {
+    vector<int> arr = {1, 3, 2, 4};
+    vector<int> ans = nextGreaterElement(arr);
+
+    for (int x : ans) cout << x << " ";
+    return 0;
+}
