5c7afc06907d3f2c46782c0ef37548c2ac2d40ec
2025b01010374 <ritu.25b01010374@abes.ac.in>
2026-09-14 12:28:46 +0530

    Pointer

--- Sem-III/OOPS/pointer.cpp
+++ Sem-III/OOPS/pointer.cpp
@@ -1,46 +1,60 @@
 #include <bits/stdc++.h>
 using namespace std;
-//                      //This Pointer
-// class Box{
-//     private:
-//      int l,b,h;
-//     public:
-//     //  void setDimension(int x, int y, int z)
-//      void setDimension(int l, int b, int h)
-//      {
-//         // l=x,b=y,h=z;
-//         // l=l, b=b, h=h;
-//         this->l=l, this->b=b, this->h=h; //this pointer (refer current object)
-//      }
+// //                      //This Pointer
+// // class Box{
+// //     private:
+// //      int l,b,h;
+// //     public:
+// //     //  void setDimension(int x, int y, int z)
+// //      void setDimension(int l, int b, int h)
+// //      {
+// //         // l=x,b=y,h=z;
+// //         // l=l, b=b, h=h;
+// //         this->l=l, this->b=b, this->h=h; //this pointer (refer current object)
+// //      }
 
-//      void showDimension()
-//      {
-//         cout << l << endl << b << endl << h;
-//      }
-// };
+// //      void showDimension()
+// //      {
+// //         cout << l << endl << b << endl << h;
+// //      }
+// // };
+
+// // int main()
+// // {
+// //     Box *p, smallBox;
+// //     p=&smallBox;
+// //     p->setDimension(5,10,15);
+// //     p->showDimension();
+
+// // }
+
+// //Create a counter class with a private account member
+// //Display method that show the count using this pointer.
+
+
+//                         //New Keyword
+   
 
 // int main()
 // {
-//     Box *p, smallBox;
-//     p=&smallBox;
-//     p->setDimension(5,10,15);
-//     p->showDimension();
+//    int a = 10;
+//    // {
+//    //    int a = 50;
+//    // }
 
+//    cout << a; //error: ‘a’ was not declared in this scope
 // }
 
-//Create a counter class with a private account member
-//Display method that show the count using this pointer.
-
-
-                        //New Keyword
-   
+int *fun()
+{
+   int num = 10;
+   return &num;
+}
 
 int main()
 {
-   int a = 10;
-   // {
-   //    int a = 50;
-   // }
-
-   cout << a; //error: ‘a’ was not declared in this scope
+   int *ptr = NULL;
+   ptr = fun();
+   cout << *ptr;
+   return 0;
 }
